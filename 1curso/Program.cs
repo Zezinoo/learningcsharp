84f556ead275ea87ba5bff690de3546d0f31be76
Jose Alves <josevitoralves561@gmail.com>
2023-05-28 23:00:37 -0300

    Add existing project files to github

---
+++ 1curso/Program.cs
@@ -0,0 +1,50 @@
+﻿//Menu options
+string ShowBandMessage = "Digite 1 para registar uma banda";
+string ListBandMessage = "Digite 2 para mostrar a lista de bandas";
+string ExitAppMessage = "Digite -1 para sair"; 
+
+string welcome_msg = "Boas vindas ao meu app";
+string logo = @"
+╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╭╮
+╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╭╯╰╮
+╭━━┳━━┳━┳━━┳━━┳━╮╱╭╮╭┳━━┳━┻╮╭╋━━┳━╮
+┃━━┫╭━┫╭┫┃━┫┃━┫╭╮╮┃╰╯┃╭╮┃━━┫┃┃┃━┫╭╯
+┣━━┃╰━┫┃┃┃━┫┃━┫┃┃┃┃┃┃┃╭╮┣━━┃╰┫┃━┫┃
+╰━━┻━━┻╯╰━━┻━━┻╯╰╯╰┻┻┻╯╰┻━━┻━┻━━┻╯";
+
+void WelcomeUser()
+{
+    Console.WriteLine(logo);
+    Console.WriteLine(welcome_msg);
+}
+
+void MenuOptions()
+{
+    Console.WriteLine(ShowBandMessage);
+    Console.WriteLine(ListBandMessage);
+    Console.WriteLine(ExitAppMessage);
+    
+    Console.Write("\nDigite a sua opção:");
+    string CurrentOption = Console.ReadLine()!;
+    int numCurrentOption = int.Parse(CurrentOption);
+    
+}
+void ChooseOption(int CurrentOption)
+{
+    switch(CurrentOption){
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        case 5:
+            break;
+        case -1:
+            break;
+        default: Console.WriteLine("Opção Invalida");break;
+    }
+        
+};
