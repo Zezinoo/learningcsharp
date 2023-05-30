01badc251cd0b08d4756522219a67cb285eb58f6
Jose Alves <josevitoralves561@gmail.com>
2023-05-30 00:09:37 -0300

    Function Implementation Added registerBand , showBand

--- 1curso/Program.cs
+++ 1curso/Program.cs
@@ -12,14 +12,22 @@ string logo = @"
 ┣━━┃╰━┫┃┃┃━┫┃━┫┃┃┃┃┃┃┃╭╮┣━━┃╰┫┃━┫┃
 ╰━━┻━━┻╯╰━━┻━━┻╯╰╯╰┻┻┻╯╰┻━━┻━┻━━┻╯";
 
+List<string> bandsList = new List<string>{"TTNG","Wulfpeck"};
+
+
+
 void WelcomeUser()
 {
-    Console.WriteLine(logo);
     Console.WriteLine(welcome_msg);
+}
+void showLogo()
+{
+    Console.WriteLine(logo);
 }
 
 void MenuOptions()
 {
+    showLogo();
     Console.WriteLine(ShowBandMessage);
     Console.WriteLine(ListBandMessage);
     Console.WriteLine(ExitAppMessage);
@@ -27,14 +35,17 @@ void MenuOptions()
     Console.Write("\nDigite a sua opção:");
     string CurrentOption = Console.ReadLine()!;
     int numCurrentOption = int.Parse(CurrentOption);
+    ChooseOption(numCurrentOption);
     
 }
 void ChooseOption(int CurrentOption)
 {
     switch(CurrentOption){
         case 1:
+            RegisterBand();
             break;
         case 2:
+            ShowBand();
             break;
         case 3:
             break;
@@ -48,3 +59,38 @@ void ChooseOption(int CurrentOption)
     }
         
 };
+
+void RegisterBand()
+{
+    Console.Clear();
+    Console.WriteLine("Registro de bandas");
+    Console.WriteLine("Digite o nome da banda a registar:");
+    string bandName = Console.ReadLine()!;
+    bandsList.Add(bandName);
+    Console.WriteLine($"A banda {bandName} foi registrada");
+    Thread.Sleep(2000);
+    Console.Clear();
+    MenuOptions();
+}
+
+void ShowBand()
+{
+    Console.Clear();
+    Console.WriteLine("Exibindo bandas registadas");
+//    for(int i = 0; i < bandsList.Count; i++)
+//    {
+//        Console.WriteLine($"Banda : {bandsList[i]}");
+//    }
+    foreach(string band in bandsList)
+    {
+        Console.WriteLine($"Banda: {band}");
+    }
+    Console.WriteLine("\nDigite qualquer tecla para voltar ao menu");
+    Console.ReadKey();
+    Console.Clear();
+    MenuOptions();
+
+}
+
+WelcomeUser();
+MenuOptions();
